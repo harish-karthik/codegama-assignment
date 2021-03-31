6b1b9a7e7d5ea6c6347ceb672c8775f84c56c199
Harish Karthik <hhkk0010@outlook.com>
2021-03-31 19:35:54 +0530

    Add browserrouter to index  and add redirect to home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import Routes from "./routes/Routes";
 
 function App() {
-  return (
-    <div className='App'>
-      <Routes />
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../pages/Home";
 import Restaurant from "../pages/Restaurant";
 function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact>
-          <Home />
-        </Route>
-        <Route path='/restaurant/:restaurantId' exact>
-          <Restaurant />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path='/' exact>
+        <Home />
+      </Route>
+      <Route path='/restaurant/:restaurantId' exact>
+        <Restaurant />
+      </Route>
+      <Route path='/'>
+        <Redirect to='/' />
+      </Route>
+    </Switch>
   );
 }
 export default Routes;
