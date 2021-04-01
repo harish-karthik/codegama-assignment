60088e93bacf44145fc90fccbda6276b6f062a05
Harish Karthik <hhkk0010@outlook.com>
2021-04-01 18:45:56 +0530

    display restaurant name and menu

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -6,7 +6,7 @@ function Restaurant() {
   let { restaurantId } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [restaurantInfo, setRestaurantInfo] = useState({});
-  const [menuItems, setMenuItems] = useState({});
+  const [menuList, setMenuList] = useState({});
   useEffect(() => {
     fetch(`${searchByIdUrl + restaurantId}`, requestOptions)
       .then((response) => response.json())
@@ -15,25 +15,36 @@ function Restaurant() {
           name: result.result.restaurant_name,
           address: { ...result.result.address },
         };
-        const resultMenuList = result.result.menus.map((menu) =>
-          menu.menu_sections.map((menuSection) => {
-            return {
+        const resultMenuList = [];
+        result.result.menus.forEach((menu) =>
+          menu.menu_sections.forEach((menuSection) => {
+            resultMenuList.push({
               name: menuSection.section_name,
               menuItems: [...menuSection.menu_items],
-            };
+            });
           })
         );
         setRestaurantInfo({ ...resultRestaurantInfo });
-        setMenuItems([...resultMenuList]);
+        setMenuList([...resultMenuList]);
         setIsLoading(false);
       });
-  }, []);
+  }, [restaurantId]);
   if (isLoading) {
     return <LoadingAnimation />;
   }
   return (
     <div>
       <h1>{restaurantInfo.name}</h1>
+      {menuList.map((menuSection) => (
+        <React.Fragment key={Math.random()}>
+          <p>{menuSection.name}</p>
+          <ul>
+            {menuSection.menuItems.map((menuItem) => (
+              <li key={Math.random()}>{menuItem.name}</li>
+            ))}
+          </ul>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
