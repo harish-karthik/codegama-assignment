72079dbbdef58651ae1873f76dfc3b86139563aa
Harish Karthik <hhkk0010@outlook.com>
2021-04-01 21:09:50 +0530

    fix cuisinelist

--- src/components/RestaurantLinks/RestaurantLinks.js
+++ src/components/RestaurantLinks/RestaurantLinks.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import CuisineList from "../CuisineList/CuisineList";
 import classes from "./RestaurantLinks.module.css";
 
 function RestaurantLinks(props) {
-  const { restaurant_name, restaurant_phone, restaurant_id, cuisines } = props;
+  const { restaurant_name, restaurant_phone, restaurant_id } = props;
   return (
     <Link
       className={classes.restaurantCard}
@@ -12,7 +11,6 @@ function RestaurantLinks(props) {
     >
       <h3 className={classes.restaurantTitle}>{restaurant_name}</h3>
       <p className={classes.restaurantContact}>{restaurant_phone}</p>
-      <CuisineList cuisines={cuisines} restaurant_id={restaurant_id} />
     </Link>
   );
 }
