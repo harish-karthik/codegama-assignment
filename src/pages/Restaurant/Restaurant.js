17deeaacc37a916349ad3e1ce4d33ac086689eb0
Harish Karthik <hhkk0010@outlook.com>
2021-04-01 18:06:21 +0530

    add states to store restaurant info and menu items

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import LoadingAnimation from "../../components/LoadingAnimation/LoadingAnimation";
 import { useParams } from "react-router-dom";
+import { searchByIdUrl, requestOptions } from "../../utilities/requestOptions";
 function Restaurant() {
   let { restaurantId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [restaurantInfo, setRestaurantInfo] = useState({});
+  const [menuItems, setMenuItems] = useState({});
+  useEffect(() => {
+    fetch(`${searchByIdUrl + restaurantId}`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        const resultRestaurantInfo = {
+          name: result.result.restaurant_name,
+          address: { ...result.result.address },
+        };
+        const resultMenuList = result.result.menus.map((menu) =>
+          menu.menu_sections.map((menuSection) => {
+            return {
+              name: menuSection.section_name,
+              menuItems: [...menuSection.menu_items],
+            };
+          })
+        );
+        setRestaurantInfo({ ...resultRestaurantInfo });
+        setMenuItems([...resultMenuList]);
+        setIsLoading(false);
+      });
+  }, []);
+  if (isLoading) {
+    return <LoadingAnimation />;
+  }
   return (
     <div>
-      <h1>Restaurant - {restaurantId}</h1>
+      <h1>{restaurantInfo.name}</h1>
     </div>
   );
 }
--- src/utilities/requestOptions.js
+++ src/utilities/requestOptions.js
@@ -2,6 +2,7 @@ export const searchByStateUrl =
   "https://api.documenu.com/v2/restaurants/search/fields?exact=true&state=";
 export const searchByNameUrl =
   "https://api.documenu.com/v2/restaurants/search/fields?exact=true&restaurant_name=";
+export const searchByIdUrl = "https://api.documenu.com/v2/restaurant/";
 let requestHeader = new Headers();
 requestHeader.append("x-api-key", "a04299b47166e37b5ddca7c872807ec0");
 export const requestOptions = {
